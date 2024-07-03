53a7afcb2d25718878dee32df998286c63605530
Guilherme Martins <robsonalmeida@MacBook-Pro-de-Guilherme.local>
2024-07-03 19:53:28 -0300

    the user can now see the data from the google sheet and delete a row

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const url =
+      "https://sheet2api.com/v1/hHc1XeB4SEea/planilha-sem-titulo/P%C3%A1gina1?";
 
+    const getData = async () => {
+      try {
+        const response = await fetch(url);
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      }
+    };
+    getData();
+  }, []);
+
+  const deleteRow = (item) => {
+    const queryParams = new URLSearchParams({
+      limit: "10",
+      query_type: "and",
+      Compra: item.Compra,
+      Categoria: item.Categoria,
+      Data: item.Data,
+      Custo: item.Custo.toString(),
+    });
+    const url =
+      "https://sheet2api.com/v1/hHc1XeB4SEea/planilha-sem-titulo/P%C3%A1gina1?" +
+      queryParams;
+
+    const deleteRowWithData = async () => {
+      try {
+        const response = await fetch(url, { method: "DELETE" });
+        const responseText = response.text();
+        console.log(responseText);
+        setData(data.filter((d) => d != item));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    deleteRowWithData();
+  };
+
+  console.log(data);
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {data ? (
+          <table>
+            <tr>
+              <th>Compra</th>
+              <th>Categoria</th>
+              <th>Custo</th>
+              <th>Data</th>
+            </tr>
+            {data.map((item, index) => (
+              <>
+                <tr>
+                <td>{item.Compra}</td>
+                <td>{item.Categoria}</td>
+                <td>{item.Custo}</td>
+                <td>{item.Data}</td>
+                <button onClick={() => deleteRow(item)}>Delete</button>
+                </tr>
+                
+              </>
+            ))}
+          </table>
+        ) : (
+          "Loading"
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
